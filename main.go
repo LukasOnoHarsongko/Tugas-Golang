8bd2fe71c6a1c198451450619f5eb501a678fcd1
LukasOnoHarsongko <lukasonoha@gmail.com>
2022-08-04 11:53:40 +0700

    1

--- main.go
+++ main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
-	"log"
-	"os"
-	"text/template"
+	"fmt"
+	"html/template"
+	"net/http"
+	"path"
 )
 
-var tpl *template.Template
-
-func init() {
-	tpl = template.Must(template.ParseFiles("tpl.html"))
-}
-
 func main() {
-	err := tpl.ExecuteTemplate(os.Stdout, "tpl.html", 42)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var filepath = path.Join("views", "index.html")
+		var tmpl, err = template.ParseFiles(filepath)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		var data = map[string]interface{}{
+			"title": "Learning Golang Web",
+			"name":  "Batman",
+		}
+
+		err = tmpl.Execute(w, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("assets"))))
+
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
 }
